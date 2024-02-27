2ff7cde202525d4f93b6d249e7bc1e1eb3157ccd
PritamGupta2000 <158317599+PritamGupta2000@users.noreply.github.com>
2024-02-27 10:30:19 +0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import reusable from "./reusable";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to My E-commerce Website</h1>
+      <Reusable />
+    </div>
+  );
+};
+
+export default App;
---
+++ reuseable_com.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./reuseable.css";
+import img from "../Assets/download.png";
+
+const Reusable = () => {
+  return (
+    <div className="container">
+      <h1>E-commerce</h1>
+      <img src={img} alt="Download" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde autem
+        natus veritatis maiores vel vero accusantium ipsum deserunt laboriosam
+        laborum? Quos eveniet enim inventore tempora illum sapiente aliquam
+        porro assumenda?
+      </p>
+      <h2>Tide Powder</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde autem
+        natus veritatis maiores vel vero accusantium ipsum deserunt laboriosam
+        laborum? Quos eveniet enim inventore tempora illum sapiente aliquam
+        porro assumenda?
+      </p>
+      <h2>Tide Powder</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde autem
+        natus veritatis maiores vel vero accusantium ipsum deserunt laboriosam
+        laborum? Quos eveniet enim inventore tempora illum sapiente aliquam
+        porro assumenda?
+      </p>
+    </div>
+  );
+};
+
+export default Reusable;
